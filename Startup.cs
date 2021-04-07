c0599314ceedd5e7b5488afc5a5a9c39e98ccc99
unknown <02160001022@ogr.inonu.edu.tr>
2021-04-07 21:57:22 +0300

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Havaalani.Startup))]
+namespace Havaalani
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
